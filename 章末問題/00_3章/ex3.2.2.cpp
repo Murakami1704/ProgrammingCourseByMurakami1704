078b0b6f41453d47c43f8fad7efe868bedd3188e
Murakami1704 <154502901+Murakami1704@users.noreply.github.com>
2026-04-19 20:41:18 +0900

    3.2.2修正
    一般性を持たせるために少し修正
    いきなり謎の学科名見せられても分からんやろ

--- 章末問題/00_3章/ex3.2.2.cpp
+++ 章末問題/00_3章/ex3.2.2.cpp
@@ -1,8 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(void){
-    cout << "氏名:情シス 太郎" << endl;
+    cout << "氏名:佐藤 太郎" << endl;
     cout << "クラス:6組" << endl;
-    cout << "学科:建設機械電気電子情報環境工学科" << endl;
+    cout << "学科:普通科" << endl;
     return 0;
 }
